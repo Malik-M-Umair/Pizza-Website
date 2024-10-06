64eb18800af750a1d9c9971252b26e51f7ca3e62
Umair <malikMuhammadUmair6@gmail.com>
2024-10-06 01:02:46 -0700

    added

--- index.js
+++ index.js
@@ -10,10 +10,10 @@
 
     }
   let index=0;
-  addEventListener('click',()=>{
+  function  changecolor(){
     let colors=["cyan","lightblue","lightgreen","grey","purple"];
     document.getElementsByTagName('body')[0].style.background=colors[index++];
    if(index>colors.length-1){
     index=0;
    }
-  })
+  }
