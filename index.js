9544d0c41f6165b713057d0934776d319f03ae99
Umair <malikMuhammadUmair6@gmail.com>
2024-10-13 06:36:46 -0700

    added

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
-let index=0;
-    function showSidebar(){
-      const sidebar=document.querySelector('.sidebar')
-      sidebar.style.display='flex'
+let index = 0;
+let classname = document.querySelectorAll('.description'); 
+let value=document.querySelectorAll('description').innerHTML;
 
-    }
-    function hideSidebar(){
-      const sidebar=document.querySelector('.sidebar')
-      sidebar.style.display='none';
 
-    }
-  
-  function  changecolor(){
-    let colors=["cyan","lightblue","lightgreen","grey","purple"];
-    document.getElementsByTagName('body')[0].style.background=colors[index++];
-   if(index>colors.length-1){
-    index=0;
-   }
+function showSidebar() {
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'flex';
+}
+
+function hideSidebar() {
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'none';
+}
+
+function changecolor() {
+  let colors = ["cyan", "lightblue", "lightgreen", "grey", "purple"];
+  document.body.style.background = colors[index++];
+  if (index > colors.length - 1) {
+    index = 0;
   }
+}
+
+
+classname.forEach(classnam => {
+  classnam.addEventListener('mouseover', function () {
+    this.innerHTML = "Pizza Size:....<br>Pizza Rate:...<br>Pizza Flavour:...."; 
+  });
+});
+classname.forEach(classnam => {
+  classnam.addEventListener('mouseout', function () {
+    this.innerHTML = "Add to Cart"; 
+  });
+});
